7627d1ea6041e2b717d1dd0d7bc70730fdce8820
Igor Tomov <itomov@griddynamics.com>
2018-06-05 13:49:01 +0300

    Added usage of new Configuration Manager.

--- bin/www.js
+++ bin/www.js
@@ -1,21 +1,28 @@
 #!/usr/bin/env node
 const path = require('path');
-const cliArgs = require('cli-args');
 const { initSingleApp } = require('ui-platform-launcher/dist/lib/app/init-app');
 const { resolveAppVersion } = require('ui-platform-core/dist/lib/path-resolvers/app-version.resolver');
 const launchApp = require('ui-platform-launcher/dist/bin/www');
 
-const args = cliArgs(process.argv.slice(2));
-const rootDir = path.join(__dirname, '..');
-const configPath = args.c;
+//const args = argv(process.argv.slice(2));
+const args = require('minimist')(process.argv.slice(2));
 
-if (! configPath) {
-  console.warn('`-c` argument with configuration filename is not passed, so default config is going to be used');
-}
+const rootDir = path.join(__dirname, '..');
+
+// set specific config reader data
+// todo: find better solution for `configReaderParams` definition
+
+
+const configReaderType = args.t || 'env-vars';
+const configReaderParams = {
+  configDirPath: args.c || 'config/'
+};
+
 
 
 launchApp(initSingleApp({
   version: resolveAppVersion(rootDir),
   rootDir,
-  configPath,
+  configReaderType,
+  configReaderParams,
 }));
