c4608739102ddf15f8647536af6ebdde1aced389
Igor Tomov <stepennwolf@gmail.com>
2018-06-05 17:04:21 +0300

    Revert "First version of SVG sprite created. (#4)"
    This reverts commit c5247aa3e68dab8bca3b14db43e49e04766523ab.

--- webpack/manifest-config.js
+++ webpack/manifest-config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
 exports.BUILD_MANIFEST_FILENAME = '../../build-manifest.json';
-exports.IMG_ASSETS_MANIFEST_FILENAME = '../../img-assets-manifest.json';
 exports.OUTPUT_MANIFEST_FILENAME = path.resolve(__dirname, '../build-manifest.json');
-
 exports.PUBLIC_PATH = '';
