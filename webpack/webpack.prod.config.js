c5247aa3e68dab8bca3b14db43e49e04766523ab
lidak <olidak@griddynamics.com>
2018-06-05 15:33:07 +0300

    First version of SVG sprite created. (#4)
    * First version of a sprite created.

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,12 +1,27 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const ExtractSVGPlugin = require('svg-sprite-loader/plugin');
 const {
   BUILD_MANIFEST_FILENAME,
-  PUBLIC_PATH
+  PUBLIC_PATH,
 } = require('./manifest-config');
 
+const outputSvgSpriteFilename = '[name].[hash:6].svg';
+
 module.exports = (coreConfig) => {
+  const SVGSpriteRule = coreConfig.module.rules.find(rule => rule.use && rule.use[0] === 'svg-sprite-loader');
   const styleLoadersRule = coreConfig.module.rules.find(rule => rule.use && rule.use[0] === 'style-loader');
+
+  SVGSpriteRule.use = [
+    {
+      loader: 'svg-sprite-loader',
+      options: {
+        extract: true,
+        filename: 'sprite.[hash].svg'
+      }
+    }
+  ];
+  styleLoadersRule.use[0] = MiniCssExtractPlugin.loader;
   // Add ExtractTextPlugin for styles
   const styleExtractFile = new MiniCssExtractPlugin({
     filename: '[name].[hash].css',
@@ -20,10 +35,12 @@ module.exports = (coreConfig) => {
       fileName: BUILD_MANIFEST_FILENAME,
       basePath: PUBLIC_PATH,
       stripSrc: true
+    }),
+
+    new ExtractSVGPlugin(outputSvgSpriteFilename, {
+      allChunks: true
     })
   );
-
-  styleLoadersRule.use[0] = MiniCssExtractPlugin.loader;
 
   return coreConfig;
 };
