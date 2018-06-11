12f31e9739ac71035d33be136700bc03ae6db770
Igor Tomov <itomov@griddynamics.com>
2018-06-11 19:43:45 +0300

    Updated configs and `www.js` according to new Configuration manager API.

--- bin/www.js
+++ bin/www.js
@@ -1,28 +1,25 @@
 #!/usr/bin/env node
 const path = require('path');
+const appName = require('../package').name;
 const { initSingleApp } = require('ui-platform-launcher/dist/lib/app/init-app');
 const { resolveAppVersion } = require('ui-platform-core/dist/lib/path-resolvers/app-version.resolver');
 const launchApp = require('ui-platform-launcher/dist/bin/www');
 
-//const args = argv(process.argv.slice(2));
 const args = require('minimist')(process.argv.slice(2));
-
 const rootDir = path.join(__dirname, '..');
 
-// set specific config reader data
-// todo: find better solution for `configReaderParams` definition
 
-
-const configReaderType = args.t || 'env-vars';
-const configReaderParams = {
-  configDirPath: args.c || 'config/'
-};
+if (! args['config-dir']) {
+  throw new Error('[www.js] `--config-type` isn\'t provided');
+}
 
 
 
-launchApp(initSingleApp({
+initSingleApp({
   version: resolveAppVersion(rootDir),
   rootDir,
-  configReaderType,
-  configReaderParams,
-}));
+  configReaderType: args['config-type'],
+  appName,
+})
+  .then(app => launchApp(app))
+  .catch(err => console.error(err));
